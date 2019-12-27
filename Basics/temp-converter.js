86abccf560b3aa559f32a3821751bcf8733df67d
Seguya Nicholus <seguyanicholus2015@gmail.com>
2019-12-27 13:45:03 +0300

    Temprature converter challenge

---
+++ Basics/temp-converter.js
@@ -0,0 +1,10 @@
+ let fahrenheit = 0 // 0 in C and 273.15 in K
+
+ //Calclate in Celcius
+ let celcius = (fahrenheit - 32)*5/9
+ console.log(celcius)
+
+
+ //Calclate in Kelvin
+ let kelvin = (fahrenheit + 459.67)* 5/9
+ console.log(kelvin)
