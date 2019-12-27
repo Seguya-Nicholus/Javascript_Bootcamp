2ceaf0ca6a84ca409700ae03daf6d87e1a920c0e
Seguya Nicholus <seguyanicholus2015@gmail.com>
2019-12-27 05:59:32 +0300

    How to use variables and Strings in Javascript

---
+++ Basics/strings.js
@@ -0,0 +1,17 @@
+//First Example
+let name = 'Seguya Nicholus'
+console.log(name)
+
+
+//Second Example
+let firstName = 'Daniel'
+let lastName = 'Lubega'
+let fullName = firstName + '' + lastName
+console.log(fullName)
+
+
+//Challenge 1
+let city = 'Entebbe'
+let country = 'Uganda'
+let location = city + ', ' + country
+console.log(location)
