38229a90d5e5d21a8bc4a30caa677a979dd8abd5
Seguya Nicholus <seguyanicholus2015@gmail.com>
2019-12-27 14:47:04 +0300

    How to use booleans and comparison operator

---
+++ Basics/boolean.js
@@ -0,0 +1,16 @@
+// === - equality operartor
+// !== - not equal operator
+// < - less than operator
+// > - greater than operator
+
+let tempK = 31
+let isFreezing = tempK === 31
+console.log(isFreezing)
+
+
+// Challenge area
+let age = 32
+let isChild = age <= 7;
+console.log(isChild)
+let isSenior = age >= 65;
+console.log(isSenior)
