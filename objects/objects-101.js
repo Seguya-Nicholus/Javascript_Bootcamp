1582a35944069ba66b6f848d2a30d54fe8962f8a
Seguya Nicholus <seguyanicholus2015@gmail.com>
2019-12-31 14:18:28 +0300

    How to define an object ,read object properties as well as changing object properties

---
+++ objects/objects-101.js
@@ -0,0 +1,11 @@
+let myPerson = {
+    name:'Seguya Nicholus',
+    age:32,
+    location:'Zziru, Kawuku Entebbe'
+} 
+
+console.log(`${myPerson.name} is ${myPerson.age} and lives in ${myPerson.location}`)
+
+myPerson.age = myPerson.age + 1
+
+console.log(`${myPerson.name} is ${myPerson.age} and lives in ${myPerson.location}`)
