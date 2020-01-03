261df7df5860a8312ca26535563171af9de99ab6
Seguya Nicholus <seguyanicholus2015@gmail.com>
2020-01-03 12:36:32 +0300

    How var differs from the let key word

---
+++ objects/var.js
@@ -0,0 +1,14 @@
+// firstName = 'Nicholus'
+
+// console.log(firstName)
+// var firstName
+
+function greet() {
+
+    let name = 'Nicholus'
+    return 'Hello ' + name
+
+    
+}
+
+console.log(greet())
